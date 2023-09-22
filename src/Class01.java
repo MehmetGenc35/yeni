de235a272b1f7b12e528d036830fa9f82c8b09aa
Mehmet Genç <gencaslan555@gmail.com>
2023-09-22 15:24:18 +0300

    Versiyon 2

--- src/Class01.java
+++ src/Class01.java
@@ -1,10 +1,13 @@
 public class Class01 {
 
     public static void main(String[] args) {
-
+        //versiyon 1
         int x=1;
         int y=2;
         System.out.println(x+y);
+        //versiyon 2
+        int z=5;
+        System.out.println(x+y+z);
 
 
     }
