588cf9554be17168c546b0d862a0c20e13b97c13
Mehmet Genç <gencaslan555@gmail.com>
2023-10-06 15:12:42 +0300

    v8

---
+++ src/Sorular/Soru.java
@@ -0,0 +1,90 @@
+package Sorular;
+
+import java.sql.SQLOutput;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Soru {
+    /*
+    ATM
+        Kullaniciya giriş için kart numarasi ve şifresini isteyin, eger herhangi birini yanlis girerse tekrar isteyin.
+        Kart numarasi aralarda boşluk ile girerse de eger doğruysa kabul edin.
+        Kart numarasi ve sifre dogrulanirsa kullanicinin yapabileceği işlemleri ekrana yazdirin,
+        Bakiye sorgula, para yatirma, para çekme, para gönderme, sifre değiştirme ve cikis.
+        Para çekme ve para gonderme işleminde mevcut bakiyeden buyuk para çekilemez,
+        Para gönderme işleminde istenen iban TR ile baslamali ve toplam 26 karakterli olmali, eger değilse menü ekranina geri donsun.
+        Sifre değiştirme işleminde mevcut şifreyi teyit ettikten sonra, sifre değişiklik işlemini yapmali,
+
+     */
+
+
+    static Scanner inp=new Scanner(System.in);
+    static SoruService soruService=new SoruService();
+    public static void main(String[] args) {
+
+        start();
+
+
+
+    }
+
+    private static void start() {
+        boolean menuyeGecis=false;
+
+        do{
+            System.out.println("Lütfen kart numaranızı giriniz: ");
+            String cardNumber=inp.nextLine().replace(" ","");
+            System.out.println("Lütfen şifrenizi giriniz: ");
+            String cardPassword=inp.nextLine();
+            if (soruService.userList("2545365424523254","qwert12345.").containsKey(cardNumber)&&soruService.userList("2545365424523254","qwert12345.").containsValue(cardPassword)){
+                int option=-1;
+                do{
+                    System.out.println("İşlem menüsüne hoşgeldiniz,yapmak istediğiniz işlemi seçiniz: ");
+                    System.out.println("1-Bakiye sorgula: \n2-Para yatirma: \n3-Para çekme: \n4-Para gönderme: \n5-Şifre değiştirme: \n0-ÇIKIŞ");
+                    int selection= inp.nextInt();
+                    switch (selection){
+                        case 1:
+                            soruService.bakiyeSorgula(soruService);
+                            break;
+                        case 2:
+                            soruService.paraYatırma(soruService);
+                            break;
+                        case 3:
+                            soruService.paraCekme(soruService);
+                            break;
+                        case 4:
+                            //Para gönderme
+                            soruService.paraGonder(soruService);
+                            break;
+                        case 5:
+                            //Şifre değiştirme
+                            soruService.sifreDegistir();
+
+                            break;
+                        case 0:
+                            //Çıkış
+                            break;
+                        default:
+                            System.out.println("Hatalı giriş yaptınız...");
+                            break;
+                    }
+                    System.out.println("Yeni bir işlem yapmak istiyor musunuz: \n1-EVET 0-HAYIR");
+                    option=inp.nextInt();
+
+                }while(option!=0);
+                System.out.println("Ana menüye yönlendiriliyorsunuz...");
+            }else{
+                System.out.println("Lütfen bilgilerinizi kontrol ediniz...");
+            }
+            System.out.println("Çıkış için 0 giriniz..." );
+            int cıkıs=inp.nextInt();inp.nextLine();
+            menuyeGecis= cıkıs==0 ? false : true;
+
+        }while(menuyeGecis);
+        System.out.println("İyi günler dileriz...");
+
+    }
+
+
+}
---
+++ src/Sorular/SoruService.java
@@ -0,0 +1,154 @@
+package Sorular;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class SoruService {
+    static Scanner inp=new Scanner(System.in);
+    private HashMap<String,Integer> bakiye =new HashMap<>();
+    private String userCard="2545365424523254";
+    private String userPassword="qwert12345.";
+    private HashMap<String,String>user=new HashMap<>();
+
+/*
+public HashMap<String,String> userList(String userCard,String userPassword){
+        userCard="2545365424523254";
+        userPassword="qwert12345.";
+        this.userCard=userCard;
+        this.userPassword=userPassword;
+        this.user.put(userCard,userPassword);
+        return user;
+    }
+ */
+
+    public SoruService() {
+    this.bakiye.put("bakiye",1000);
+
+    }
+    public HashMap<String,String> userList(){
+        this.user.put(userCard,userPassword);
+        return user;
+    }
+
+    public void bakiyeSorgula(SoruService soruService) {
+
+        System.out.println("Mevcut bakiyeniz: "+soruService.bakiye.get("bakiye"));
+
+    }
+
+    public void paraYatırma(SoruService soruService) {
+
+        boolean secim =false;
+        int money;
+        int yeniBakiye;
+        do {
+            System.out.println("Ne kadar para yatırmak istersiniz: ");
+            money=inp.nextInt();
+
+            if(money>0){
+                System.out.println("Yatırmak istediğiniz tutar "+money+" liradır.");
+                System.out.println("Para eklemek istiyor musunuz: \n1-EVET\n2-HAYIR");
+                int tercih= inp.nextInt();
+                secim= tercih==1? true : false;
+
+            }
+            yeniBakiye= soruService.bakiye.get("bakiye")+money;
+            soruService.bakiye.clear();
+            soruService.bakiye.put("bakiye",yeniBakiye);
+        }while (secim);
+
+        bakiyeSorgula(soruService);
+
+    }
+
+    public void paraCekme(SoruService soruService) {
+        boolean secim =false;
+        int money;
+        int yeniBakiye;
+        do {
+            System.out.println("Ne kadar para çekmek istersiniz: ");
+            money=inp.nextInt();
+
+            if(money<soruService.bakiye.get("bakiye")){
+
+                System.out.println("Çekmek istediğiniz tutar "+money+" liradır.");
+
+                yeniBakiye= soruService.bakiye.get("bakiye")-money;
+                soruService.bakiye.clear();
+                soruService.bakiye.put("bakiye",yeniBakiye);
+
+                bakiyeSorgula(soruService);
+            }else{
+                System.out.println("Hesabınızda yeterli miktar bulunamadı...");
+            }
+            System.out.println("Tekrar para çekmek istiyor musunuz: \n1-EVET\n2-HAYIR");
+            int tercih= inp.nextInt();
+            secim= tercih==1? true : false;
+
+
+        }while (secim);
+
+
+    }
+
+    public void paraGonder(SoruService soruService) {
+        boolean secim =false;
+        int money;
+        int yeniBakiye;
+        do {
+            System.out.println("Para gönderilecek IBAN hesabını giriniz: ");
+            String paraGidecekHesap=inp.nextLine();
+            if (paraGidecekHesap.substring(0,2).equals("TR")&& paraGidecekHesap.length()==26){
+                System.out.println("Ne kadar para göndermek istersiniz: ");
+                money=inp.nextInt();
+
+                if(money<=soruService.bakiye.get("bakiye")){
+
+                    System.out.println("Göndermek istediğiniz tutar "+money+" liradır.");
+
+                    yeniBakiye= soruService.bakiye.get("bakiye")-money;
+                    soruService.bakiye.clear();
+                    soruService.bakiye.put("bakiye",yeniBakiye);
+
+                    System.out.println("Gönderme işlemi başarılı...");
+                    bakiyeSorgula(soruService);
+                }else{
+                    System.out.println("Hesabınızda yeterli miktar bulunamadı...");
+                }
+                System.out.println("Tekrar para göndermek istiyor musunuz: \n1-EVET\n2-HAYIR");
+                int tercih= inp.nextInt();inp.nextLine();
+                secim= tercih==1? true : false;
+            }else{
+                System.out.println("Gönderilecek hesap 'TR' ile başlamalı ve toplam 26 haneli olmalıdır... ");
+                secim=true;
+            }
+
+        }while (secim);
+
+
+
+    }
+
+
+    public void sifreDegistir() {
+        int secim;
+        do {
+            System.out.println("Eski şifrenizi giriniz: ");
+            String oldPassword=inp.nextLine();
+            if(oldPassword.equals(this.userList().get("2545365424523254"))){
+                System.out.println("Yeni şifrenizi giriniz: ");
+                String newPassword=inp.nextLine();
+
+                this.userList().replace("2545365424523254", oldPassword ,newPassword);
+
+                System.out.println("Şifreniz güncellenmiştir...");
+                secim=0;
+            }else{
+                System.out.println("Şifrenizi kontrol ediniz...");
+                secim=1;
+            }
+
+        }while(secim!=0);
+
+    }
+}
